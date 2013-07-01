312231424cc73791f1f4275ef58db122886074ac
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2013-07-01 21:57:56 +0900

    とりあえず完成

--- lib/to_koike_kazuo/extention/object.rb
+++ lib/to_koike_kazuo/extention/object.rb
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 class String
+  
   def to_koike_kazuo
      replaces =  Hash::new
      replaces["ん"] = "ン"
      replaces["勃起"] = "エレクチオン"
      replaces["ちきしょーッ"] = "王八蛋"
-     replaces["女"] = "オンナ"
-     
-
 
      tmp = self
      replaces.each_pair {|key, value| 
--- spec/extention/object_spec.rb
+++ spec/extention/object_spec.rb
@@ -1,10 +1,17 @@
 # -*- encoding: UTF-8 -*-
 require File.expand_path(File.join('../', 'spec_helper'), File.dirname(__FILE__))
 
-describe "test" do 
+describe "ん->ン" do 
     it "Convert one character"  do expect("へん".to_koike_kazuo).to eq "へンッ";  end
     it "Convert multiple characters"  do expect("文字列へんかん".to_koike_kazuo).to eq "文字列へンかンッ";  end  
-    it "Convert 勃起"  do expect("勃起".to_koike_kazuo).to eq "エレクチオンッ";  end
-    it "Convert "  do expect("ちきしょーッ なんで勃起しない！！".to_koike_kazuo).to eq "王八蛋 なンでエレクチオンしない！！ッ"; end
-        
 end
+
+describe "勃起->エレクチオン" do 
+    it "Convert 勃起"  do expect("勃起".to_koike_kazuo).to eq "エレクチオンッ";  end
+    it "Convert x勃起x"  do expect("x勃起x".to_koike_kazuo).to eq "xエレクチオンxッ";  end
+end
+
+describe "text" do 
+    it "Convert Text"  do expect("ちきしょーッ なんで勃起しない！！".to_koike_kazuo).to eq "王八蛋 なンでエレクチオンしない！！ッ"; end
+end
+
